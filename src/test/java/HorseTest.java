d082b2aa98ff288c0a2053fb53e656496f275d28
Tony <anton.rymarenko@gmail.com>
2025-02-21 14:28:10 +0200

    add hipodrome tests,main tests, update junit version

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,13 +1,12 @@
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class HorseTest {
