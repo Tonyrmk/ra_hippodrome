9067ba5eb4d896c06d51ab5eb3a82e76534d5807
Tony <anton.rymarenko@gmail.com>
2025-02-21 16:55:53 +0200

    [update] add logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import lombok.extern.log4j.Log4j2;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+@Log4j2
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -14,6 +16,7 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        log.info("Jumps are started, participants count is {}", horses.size());
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
@@ -23,6 +26,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        log.info("Jumps have finished. The winner is {}", winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
